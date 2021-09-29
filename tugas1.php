017076a97e5bf1ba63c88daa094e2abb05325e4c
Rafi Muhammad <rafi.muhammad001@binus.ac.id>
2021-09-29 13:41:32 +0700

    first commit

---
+++ tugas1.php
@@ -0,0 +1,72 @@
+<?php  
+  // Nomor 1
+  $ayam_goreng = 13000;
+  $ayam_bakar = 15000;
+  $ayam_sayur = 13000;
+  $tempe_goreng = 1000;
+  $tahu_goreng = 1000;
+  $soto = 3000;
+  $nasi_putih = 5000;
+  $es_teh = 4000;
+  $es_tebu = 5000;
+
+  echo "<h1>Daftar Menu</h1>";
+  echo ("
+    <li>Ayam Goreng  = Rp. $ayam_goreng</li>
+    <li>Ayam Bakar   = Rp. $ayam_bakar</li>
+    <li>Ayam Sayur   = Rp. $ayam_sayur</li>
+    <li>Tempe Goreng = Rp. $tempe_goreng</li>
+    <li>Tahu Goreng  = Rp. $tahu_goreng</li>
+    <li>Soto         = Rp. $soto</li>
+    <li>Nasi Putih   = Rp. $nasi_putih</li>
+    <li>Es Teh       = Rp. $es_teh</li>
+    <li>Es Tebu      = Rp. $es_tebu</li>
+  ");
+
+  // Nomor 2
+  $cireng = 1000;
+  $es_duren = 15000;
+  $es_milo = 20000;
+  $es_teh = 5000;
+  $piscok = 1000;
+
+  echo "<h1>DAFTAR MENU</h1>";
+  echo ("
+    <table>
+      <thead>
+        <tr>
+          <th>No</th>
+          <th>Nama Menu</th>
+          <th>Harga</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>1</td>
+          <td>Cireng</td>
+          <td>Rp $cireng</td>
+        </tr>
+        <tr>
+          <td>2</td>
+          <td>Es Duren</td>
+          <td>Rp $es_duren</td>
+        </tr>
+        <tr>
+          <td>3</td>
+          <td>Es Milo</td>
+          <td>Rp $es_milo</td>
+        </tr>
+        <tr>
+          <td>4</td>
+          <td>Es Teh</td>
+          <td>Rp $es_teh</td>
+        </tr>
+        <tr>
+          <td>5</td>
+          <td>Piscok</td>
+          <td>Rp $piscok</td>
+        </tr>
+      </tbody>
+    </table>
+  ")
+?>
